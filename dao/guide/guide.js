caa3fac36d995194c192016e6404127280cf7a7a
161250013 <161250013@smail.nju.edu.cn>
2018-06-02 19:48:00 +0800

    增加dao层

---
+++ dao/guide/guide.js
@@ -0,0 +1,19 @@
+const query = require('../database')
+
+let findByOpenId = function (openId) {
+    return []
+}
+
+let findById = function (id) {
+    return []
+}
+
+let findByFavorSpot = function (spotId) {
+    return []
+}
+
+let findByKeyword = function (keyword) {
+    return []
+}
+
+module.exports = {findById, findByOpenId, findByFavorSpot, findByKeyword}
---
+++ dao/order/order.js
@@ -0,0 +1,39 @@
+const query = require('../database')
+
+let findById = function (id) {
+    return []
+}
+
+let findByGuideId = function (guideId) {
+    return []
+}
+
+let findByGuideIdAndState = function (guideId, state) {
+    return []
+}
+
+let findByGuideIdAndKeyword = function (guideId, keyword) {
+    return []
+}
+
+let findByTouristId = function (touristId) {
+    return []
+}
+
+let findByTouristIdAndState = function (touristId, state) {
+    return []
+}
+
+let findByTouristAndKeyword = function (touristId, keyword) {
+    return []
+}
+
+module.exports = {
+    findById,
+    findByGuideId,
+    findByGuideIdAndState,
+    findByGuideIdAndKeyword,
+    findByTouristId,
+    findByTouristIdAndState,
+    findByTouristAndKeyword
+}
