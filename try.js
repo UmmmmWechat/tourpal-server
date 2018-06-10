0a530f288ee53e194e267082f7bf6975cc65d39d
161250013 <161250013@smail.nju.edu.cn>
2018-06-10 11:04:27 +0800

    增加郑州景点

--- try.js
+++ try.js
@@ -1,7 +1,22 @@
-let a = ''
+// let a = ''
+//
+// try {
+//     throw new Error('dkkd')
+// } catch (error) {
+//     console.log(error.message)
+// }
 
-try {
-    throw new Error('dkkd')
-} catch (error) {
-    console.log(error.message)
+const spotDao = require('./dao/spot/spot')
+
+const fs = require('./spotszz')
+
+for (let i = 0; i < fs.length; i++) {
+    let spot = fs[i]
+    spot.location = {
+        province: '河南省',
+        city: '郑州市',
+        region: ''
+    }
+    spotDao.insert(spot).then(res => {
+    }).catch(err => {})
 }
